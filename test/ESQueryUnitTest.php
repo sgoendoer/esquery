73a540d312ff260beac096e912cfd3a0240dc3a1
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2016-04-19 13:53:06 +0200

    first UnitTest code

--- test/ESQueryUnitTest.php
+++ test/ESQueryUnitTest.php
@@ -2,43 +2,22 @@
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-use sgoendoer\json\JSONObject;
-use sgoendoer\json\JSONArray;
+use sgoendoer\esquery\ESQuery;
+use sgoendoer\esquery\ESQueryBuilder;
+use sgoendoer\esquery\ESQueryException;
 
-class JSONArrayUnitTest extends PHPUnit_Framework_TestCase
+class ESQueryUnitTest extends PHPUnit_Framework_TestCase
 {
 	public function test()
 	{
-		$jsonString = '[]';
-		$jsonStringString = '["a"]';
-		$jsonStringInteger = '[2]';
-		$jsonStringBool = '[true]';
-		$jsonStringNull = '[null]';
-		$jsonStringObject = '[{}]';
-		$jsonStringArray = '[[]]';
+		$esquery = (new ESQueryBuilder())
+					->type('test')
+					->build();
+		
+		$esquery->setMatch('a', 'b');
 		
 		// serialization // creation from string
-		$this->assertEquals($jsonString, (new JSONArray())->__toString());
-		
-		$this->assertEquals(json_decode($jsonString), (new JSONArray())->toArray());
-		$this->assertEquals(json_decode($jsonStringString), (new JSONArray($jsonStringString))->toArray());
-		$this->assertEquals(json_decode($jsonStringInteger), (new JSONArray($jsonStringInteger))->toArray());
-		$this->assertEquals(json_decode($jsonStringBool), (new JSONArray($jsonStringBool))->toArray());
-		$this->assertEquals(json_decode($jsonStringNull), (new JSONArray($jsonStringNull))->toArray());
-		$this->assertEquals(json_decode($jsonStringObject), (new JSONArray($jsonStringObject))->toArray());
-		$this->assertEquals(json_decode($jsonStringArray), (new JSONArray($jsonStringArray))->toArray());
-		
-		// building arrays
-		$a = new JSONArray();
-		$a->put('x');
-		$a->put(new JSONArray());
-		
-		$a1 = '["x",[]]';
-		$this->assertEquals($a1, $a->write());
-		
-		// accessing parameters
-		$this->assertEquals("x", $a->get(0));
-		$this->assertEquals("[]", $a->get(1));
+		$this->assertEquals($esquery, ESQueryBuilder::buildFromJSON($esquery->getJSONString()));
 	}
 }
 
